1866d2019fc846ead493b3eb48eb78b66b2aac63
lkguevara <liann.guevara@gmail.com>
2023-10-05 15:55:55 -0500

    estructura del listado de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function App() {
 
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
+    <Container className='app' fluid>
+      <div className='title'>
+        <h1>To Do list</h1>
+      </div>
+
+      <Row className='todo'>
+        <Col  
+          className='todo__title'
+          xs={{span: 10, offset: 1}}
+          md={{span: 6, offset: 3}}
+        >
+          <h2>Today</h2>
+        </Col>
+        <Col  
+          className='todo__list'
+          xs={{span: 10, offset: 1}}
+          md={{span: 6, offset: 3}}
+        >
+          <p>List task</p>
+        </Col>
+        <Col  
+          className='todo__input'
+          xs={{span: 10, offset: 1}}
+          md={{span: 6, offset: 3}}
+        >
+          <input type='text' placeholder='Add a task' />
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
